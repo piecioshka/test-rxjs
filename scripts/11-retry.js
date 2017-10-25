e9b9b8bae6332917a79c5fffec324bd5ffe24d50
Piotr Kowalski <piecioshka@gmail.com>
2017-10-25 23:25:29 +0200

    Make limits of retry XHR requets

---
+++ scripts/11-retry.js
@@ -0,0 +1,34 @@
+console.log('[Testing] Observable: 11-retry');
+
+const RETRY_NUMBER = 2;
+
+function sendRequest(url) {
+    console.log('sendRequest', url);
+
+    return Rx.Observable.ajax(url)
+        .timeout(100)
+        .retryWhen((error$) => {
+            console.log('sendRequest (retry)');
+            let errorCount = 0;
+            return error$
+                .do((err) => {
+                    console.log('errorCount', errorCount);
+
+                    if (errorCount === RETRY_NUMBER) {
+                        throw err;
+                    }
+
+                    errorCount++;
+                })
+                .delay(2000)
+        });
+}
+
+sendRequest('./?block', {})
+    .subscribe((r) => {
+        console.log('SUCCESS', r)
+    }, (e) => {
+        console.log('ERROR', e)
+    }, (...args) => {
+        console.log('FINALLY', ...args)
+    });
